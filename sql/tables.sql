4576af9c11ebd5a5fc745a61e35f3cde32ae1944
Etu1906 <118957897+Etu1906@users.noreply.github.com>
2023-10-05 19:06:41 +0100

    donnee base

---
+++ sql/tables.sql
@@ -0,0 +1,26 @@
+create sequence idsession;
+
+
+
+create table session(
+    idsession varchar primary key
+);
+
+create table session_value(
+    idsession varchar references session,
+    cle varchar,
+    valeur json
+);
+
+
+insert into session values(( SELECT left(md5(random()::text), 14) || nextval('idsession')) );
+
+-- get
+insert into session_value values( '673c1de860392e2' , 'vaovao' , '{"nom": 1 }' );
+
+-- unset
+UPDATE session_value
+SET valeur = valeur::jsonb - 'nom'
+
+-- get
+SELECT valeur->>'nom' as nombre FROM session_value;
