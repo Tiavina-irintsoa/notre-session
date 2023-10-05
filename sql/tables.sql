2832252d88c0d393a9b44cead18cdb716ebfa755
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2023-10-05 19:26:11 +0200

    fix base 2

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,26 +1,17 @@
 create sequence idsession;
 
-
-
-create table session(
-    idsession varchar primary key
-);
-
 create table session_value(
-    idsession varchar references session,
-    cle varchar,
+    idsession varchar primary key,
     valeur json
 );
 
 
-insert into session values(( SELECT left(md5(random()::text), 14) || nextval('idsession')) );
-
 -- set
-insert into session_value values( '673c1de860392e2' , 'vaovao' , '{"nom": 1 }' );
+insert into session_value values( ( SELECT left(md5(random()::text), 14) || nextval('idsession'))  , '{"nom": 1 }' );
 
 -- unset
 UPDATE session_value
-SET valeur = valeur::jsonb - 'nom'
+SET valeur = valeur::jsonb - 'nom' where idsession = ....
 
 -- get
 SELECT valeur->>'nom' as nombre FROM session_value;
